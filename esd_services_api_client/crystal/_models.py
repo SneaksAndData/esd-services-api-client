3ac0c4cd35ef309f8889d1d97517474a14507720
George Zubrienko <GZU@ecco.com>
2022-09-06 15:58:14 +0200

    Support tags and custom config from the api

--- esd_services_api_client/crystal/_models.py
+++ esd_services_api_client/crystal/_models.py
@@ -3,7 +3,9 @@
 """
 from enum import Enum
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Dict, List
+
+from dataclasses_json import dataclass_json, LetterCase, DataClassJsonMixin
 
 
 class RequestLifeCycleStage(Enum):
@@ -67,3 +69,57 @@ class CrystalEntrypointArguments:
     request_id: str
     results_receiver: str
     sign_result: Optional[bool] = None
+
+
+class AlgorithmConfigurationValueType(Enum):
+    """
+      Value type for algorithm config maps and secrets.
+
+      PLAIN - plain text value
+      RELATIVE_REFERENCE - reference to a file deployed alongside algorithm config.
+    """
+    PLAIN = "PLAIN"
+    RELATIVE_REFERENCE = "RELATIVE_REFERENCE"
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class AlgorithmConfigurationEntry(DataClassJsonMixin):
+    """
+      Crystal algorithm configuration entry.
+    """
+    name: str
+    value: str
+    value_type: Optional[AlgorithmConfigurationValueType] = None
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class AlgorithmConfiguration(DataClassJsonMixin):
+    """
+     Crystal algorithm configuration. Used for overriding defaults.
+    """
+    image_repository: Optional[str] = None
+    image_tag: Optional[str] = None
+    deadline_seconds: Optional[int] = None
+    maximum_retries: Optional[int] = None
+    env: Optional[List[AlgorithmConfigurationEntry]] = None
+    secrets: Optional[List[str]] = None
+    args: Optional[List[AlgorithmConfigurationEntry]] = None
+    cpu_limit: Optional[str] = None
+    memory_limit: Optional[str] = None
+    workgroup: Optional[str] = None
+    version: Optional[str] = None
+    monitoring_parameters: Optional[List[str]] = None
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class AlgorithmRequest(DataClassJsonMixin):
+    """
+      Crystal algorthm request.
+    """
+    algorithm_name: str
+    algorithm_parameters: Dict
+    custom_configuration: Optional[AlgorithmConfiguration] = None
+    tag: Optional[str] = None
