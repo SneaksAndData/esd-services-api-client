992c22b4e466a3253ec450ec204e59c547f0fbf4
George Zubrienko <GZU@ecco.com>
2022-09-06 12:42:27 +0200

    Simplify test

--- tests/test_beast_connector.py
+++ tests/test_beast_connector.py
@@ -8,9 +8,5 @@ def test_request_ser():
     test_data_path = f"{pathlib.Path(__file__).parent.resolve()}/beast/job_request.json"
     with open(test_data_path, 'r', encoding='utf-8') as jr_ser_handle:
         jr_ser = jr_ser_handle.read()
-        try:
-            jr_dict = JobRequest.from_json(jr_ser, infer_missing=True).to_dict()
-            assert { k: v for k, v in jr_dict.items() if v is not None } == json.loads(jr_ser)
-        except Exception as ex:
-            print(ex)
-            assert False
+        jr_dict = JobRequest.from_json(jr_ser, infer_missing=True).to_dict()
+        assert {k: v for k, v in jr_dict.items() if v is not None} == json.loads(jr_ser)
