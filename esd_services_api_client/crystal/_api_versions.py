e0ad6e47f5ac785fb6e7467b90d56895315ec931
Vitaliy Savitskiy <visa@ecco.com>
2023-01-12 14:52:44 +0100

    Update esd_services_api_client/crystal/_api_versions.py
    Co-authored-by: George Zubrienko <gzu@ecco.com>

--- esd_services_api_client/crystal/_api_versions.py
+++ esd_services_api_client/crystal/_api_versions.py
@@ -8,7 +8,7 @@ class ApiVersion(Enum):
     """
       API Versions
       V1_1 - Initial version
-      V1_2 - Basic authentication replaced with Boxer token authentication
+      V1_2 - Boxer token authentication and updated API for run submission and result read
     """
     V1_1 = "v1.1"
     V1_2 = "v1.2"
