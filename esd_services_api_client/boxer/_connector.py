73187f094817d1fe860281ef95401c8f652ab0a9
Vitaliy Savitskiy <visa@ecco.com>
2023-01-23 13:01:08 +0100

    Add method to get kubernetes authentication token

--- esd_services_api_client/boxer/_connector.py
+++ esd_services_api_client/boxer/_connector.py
@@ -152,3 +152,16 @@ def select_authentication(auth_provider: str, env: str) -> Optional[BoxerTokenAu
         boxer_connector = BoxerConnector(base_url=f"https://boxer.{env}.sneaksanddata.com", auth=external_auth)
         return BoxerTokenAuth(boxer_connector)
     return None
+
+
+def get_kubernetes_token(cluster_name: str, env: str):
+    """
+    Create Boxer auth based on kubernetes cluster token for ExternalTokenAuth.
+    :param cluster_name: Name of the cluster (should match name of Identity provider in boxer configuration)
+    :param env: Boxer environment name
+    :return: BoxerTokenAuth configured fot particular identity provider and kubernetes auth token
+    """
+    with open("/var/run/secrets/kubernetes.io/serviceaccount/token", 'r', encoding='utf-8') as token_file:
+        external_auth = ExternalTokenAuth(token_file.readline(), cluster_name)
+        boxer_connector = BoxerConnector(base_url=f"https://boxer.{env}.sneaksanddata.com", auth=external_auth)
+        return BoxerTokenAuth(boxer_connector)
