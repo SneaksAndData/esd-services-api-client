a38bcc0d7f29db86d6883d85e0576d2aa3a65235
George Zubrienko <gzu@ecco.com>
2023-08-07 17:23:24 +0200

    Add support for ExtendedCodeMount submission flag

--- esd_services_api_client/beast/_connector.py
+++ esd_services_api_client/beast/_connector.py
@@ -272,6 +272,7 @@ class BeastConnector:
                 debug_mode=job_params.debug_mode,
                 expected_parallelism=job_params.expected_parallelism,
                 submission_mode=job_params.submission_mode,
+                extended_code_mount=job_params.extended_code_mount,
             )
 
             request_id, _ = self._submit(submit_request)
