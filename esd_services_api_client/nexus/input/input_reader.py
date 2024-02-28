209077125d261f9a3a4ea2b841270dccc94ddcad
George Zubrienko <gzu@ecco.com>
2024-02-28 15:26:54 +0100

    Make polars part of Nexus by default and fix data path in metrics report for the reader

--- esd_services_api_client/nexus/input/input_reader.py
+++ esd_services_api_client/nexus/input/input_reader.py
@@ -80,7 +80,9 @@ class InputReader(NexusObject[TPayload, TResult]):
 
         @run_time_metrics_async(
             metric_name="read_input",
-            on_finish_message_template="Finished reading {entity} from path {data_path} in {elapsed:.2f}s seconds",
+            on_finish_message_template="Finished reading {entity} from path {data_path} in {elapsed:.2f}s seconds"
+            if self.socket
+            else "Finished reading {entity} from path {data_path} in {elapsed:.2f}s seconds",
             template_args={
                 "entity": self.__class__.alias().upper(),
             }
