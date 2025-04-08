1a4680821e43a1f3e94fa1dfc85cb78c2c888fde
JanusAsmussen <jjasmussen@outlook.com>
2025-04-08 16:01:59 +0200

    Fix lint

--- esd_services_api_client/nexus/telemetry/user_telemetry_recorder.py
+++ esd_services_api_client/nexus/telemetry/user_telemetry_recorder.py
@@ -72,6 +72,7 @@ class UserTelemetryRecorder(Generic[TPayload, TResult], ABC):
     Base class for user-defined telemetry recorders.
     """
 
+    # pylint: disable=too-many-positional-arguments
     @inject
     def __init__(
         self,
